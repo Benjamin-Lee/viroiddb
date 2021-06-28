a85a259d167734376605a3ea514911744904a9fa
Benjamin Lee <benjamindlee@me.com>
2021-06-28 04:50:57 -0700

    Change the name

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'viroiddb',
+    title: 'ViroidDB',
     htmlAttrs: {
       lang: 'en',
     },
