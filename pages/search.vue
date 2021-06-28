d19e4fe3073e385e63f8080a24f0e9574e642209
Benjamin Lee <benjamindlee@me.com>
2021-06-28 03:47:51 -0700

    Clean up the data on the search page

--- pages/search.vue
+++ pages/search.vue
@@ -12,7 +12,7 @@
             py-2
             border-b-4 border-indigo-600
             bg-gray-50
-            text-4xl
+            text-3xl
             pl-0
             rounded-none
             focus:outline-none
@@ -28,58 +28,63 @@
 
     <ais-stats>
       <p slot-scope="{ nbHits, processingTimeMS }" class="text-indigo-600">
-        {{ nbHits }}
+        {{ Number(nbHits).toLocaleString() }}
         <span class="text-gray-900">results ({{ processingTimeMS }} ms)</span>
       </p>
     </ais-stats>
 
     <div class="lg:flex lg:justify-between mb-4">
       <div class="my-4">
-        <p class="text-indigo-600 text-xl">Type</p>
+        <p class="text-indigo-600 text-xl mb-1">Type</p>
         <ais-refinement-list attribute="type" />
       </div>
       <div class="my-4">
-        <p class="text-indigo-600 text-xl">Family</p>
+        <p class="text-indigo-600 text-xl mb-1">Family</p>
         <ais-refinement-list attribute="family" class="italic" />
       </div>
       <div class="my-4">
-        <p class="text-indigo-600 text-xl">Genus</p>
+        <p class="text-indigo-600 text-xl mb-1">Genus</p>
         <ais-refinement-list attribute="genus" class="italic" />
       </div>
       <div class="my-4">
-        <p class="text-indigo-600 text-xl">Species</p>
+        <p class="text-indigo-600 text-xl mb-1">Species</p>
         <ais-refinement-list attribute="species" class="italic" />
       </div>
     </div>
-    <ais-infinite-hits>
+    <ais-infinite-hits class="mx-1">
       <template slot="item" slot-scope="{ item }">
         <nuxt-link :to="`/sequences/${item.accession}`">
-          <p class="text-gray-900 mx-1 hover:text-indigo-600 text-lg">
+          <p class="text-gray-900 hover:text-indigo-600 text-lg font-medium">
+            <ais-highlight attribute="accession" :hit="item" />
             <ais-highlight attribute="displayTitle" :hit="item" />
           </p>
-          <p class="text-gray-500 mx-1">
-            <span class="not-italic">type:</span>
-            <ais-highlight attribute="type" :hit="item" />
+          <p class="text-gray-900">
+            <!-- <span class="text-gray-500 w-10 mr-5">Taxonomy</span> -->
+            <ais-highlight attribute="type" :hit="item" class="capitalize" />
+            <template v-if="item.family">
+              &#8250;
+              <ais-highlight attribute="family" :hit="item" class="italic" />
+            </template>
+            <template v-if="item.genus">
+              &#8250;
+              <ais-highlight attribute="genus" :hit="item" class="italic" />
+            </template>
+            <template v-if="item.species">
+              &#8250;
+              <ais-highlight attribute="species" :hit="item" class="italic" />
+            </template>
           </p>
-          <p class="text-gray-500 mx-1 italic">
-            <span class="not-italic">family:</span>
-            <ais-highlight attribute="family" :hit="item" />
-          </p>
-          <p class="text-gray-500 mx-1 italic">
-            <span class="not-italic">genus:</span>
-            <ais-highlight attribute="genus" :hit="item" />
-          </p>
-          <p class="text-gray-500 mx-1 italic">
-            <span class="not-italic">species:</span>
-            <ais-highlight attribute="species" :hit="item" />
-          </p>
-          <p class="text-gray-500 mx-1">
-            <ais-highlight attribute="accession" :hit="item" />
-          </p>
-          <p class="text-gray-500 mx-1">
+          <p class="text-gray-900">
             <ais-highlight attribute="geoLocation" :hit="item" />
+            <span v-if="item.geoLocation && item.host">&#8250;</span>
+            <ais-highlight attribute="host" :hit="item" class="italic" />
+            <span v-if="(item.geoLocation || item.host) && item.isolationSource"
+              >&#8250;</span
+            >
+            <ais-highlight attribute="isolationSource" :hit="item" />
           </p>
-          <p class="text-gray-500 mx-1">
+          <p class="text-gray-900">
+            <!-- <span class="text-gray-500">Submitters</span> -->
             <ais-highlight attribute="submitters" :hit="item" />
           </p>
         </nuxt-link>
