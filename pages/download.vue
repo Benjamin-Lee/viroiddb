9a5a25deda32e45bd9078ce0d1dc0fb87d6a287c
Caleb Oh <caleboh01@gmail.com>
2021-06-29 18:53:37 -0400

    made new download page

--- pages/download.vue
+++ pages/download.vue
@@ -1,9 +1,47 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
   <div>
-    <div class="flex">
+    <div>
+      <div class="text-3xl font-bold mb-6">Download</div>
+      <p class="has-line-data" data-line-start="0" data-line-end="2">
+        All the data displayed on this site is attainable both via the
+        individual sequence pages, and through simple
+        <code>curl</code> commands.<br />
+        This includes:
+      </p>
+      <ol>
+        <li class="has-line-data my-4" data-line-start="3" data-line-end="4">
+          1. The raw nucleic sequences, along with their related metadata, ‘as
+          is’, without any modification to their original counterparts of the
+          different public databases such as NCBI’s
+          <a href="https://www.ncbi.nlm.nih.gov/refseq/">RefSeq</a> or
+          <a href="https://www.ncbi.nlm.nih.gov/genbank/">GenBank</a>.
+        </li>
+        <li class="has-line-data my-4" data-line-start="4" data-line-end="5">
+          2. The derived information, including the analyses, outputs,
+          visualizations, etc, which we performed after initial sourcing the
+          entries detailed in This include, among else, sequence clustering (<a
+            href="https://github.com/soedinglab/mmseqs2"
+            >mmseqs</a
+          >), secondary structure prediction (<a
+            href="http://rna.tbi.univie.ac.at/cgi-bin/RNAWebSuite/RNAfold.cgi"
+            >RNAFold</a
+          >), rotational alignments (<a
+            href="https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-10-230"
+            >CSA</a
+          >), and catalytic potential of sub sequences via
+          <a href="http://eddylab.org/infernal/">infernal</a> based searches
+          using the <a href="https://rfam.org/">Rfam</a> database.
+        </li>
+      </ol>
       <div>
-        <div class="font-bold">Subset</div>
+        For ease of use, we provide a <code>curl</code> script generator to
+        download data subsets
+      </div>
+    </div>
+    <div class="flex mt-6">
+      <div class="sm:flex sm:items-center">
+        <div class="font-bold text-xl sm:mr-2">Subset</div>
         <div class="relative inline-block text-left">
           <div>
             <button
@@ -65,7 +103,7 @@
             class="
               origin-top-right
               absolute
-              right-0
+              left-0
               mt-2
               w-56
               rounded-md
@@ -157,8 +195,8 @@
           </div>
         </div>
       </div>
-      <div class="ml-4">
-        <div class="font-bold">Format</div>
+      <div class="ml-4 sm:flex sm:items-center">
+        <div class="font-bold text-xl sm:mr-2">Format</div>
         <div class="relative inline-block text-left">
           <div>
             <button
@@ -298,7 +336,16 @@
     </div>
 
     <pre
-      class="bg-gray-800 text-white p-4 mt-4 rounded max-w-7xl flex justify-between"
+      class="
+        bg-gray-800
+        text-white
+        p-4
+        mt-4
+        rounded
+        max-w-7xl
+        flex
+        justify-between
+      "
     ><div>{{ url }}</div> <button
         type="button"
         class="
