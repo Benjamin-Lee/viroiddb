c211bdf64d30f8a8874c1d189752d758ba8229a2
Benjamin Lee <benjamindlee@me.com>
2021-08-29 07:25:00 -0400

    Update build script to group data

--- scripts/build2.sh
+++ scripts/build2.sh
@@ -18,7 +18,19 @@ for d in $output/*.fasta; do
     filename="$(basename $d)"
     filename="${filename%.*}"
     RNAfold --jobs=0 --circ --temp=25 --noPS $d > $output/$filename.dbn
+    cmscan --cpu 4 ../db/raw/ribozymes.cm $d > $output/$filename.cmscan
     seqkit seq -rp $d | RNAfold --jobs=0 --circ --temp=25 --noPS > $output/$filename.rc.dbn
 done
+
+cat $output/*.fasta > $output/all.fasta
+cat $output/*.dbn > $output/all.dbn
+cat $output/*.rc.dbn > $output/all.rc.dbn
+cat $output/*.cmscan > $output/all.cmscan
+
+# Group all of the viroid data together
+cat $output/avsunviroidae.fasta $output/pospiviroidae.fasta  $output/unclassified.fasta > $output/viroids.fasta
+cat $output/avsunviroidae.dbn $output/pospiviroidae.dbn  $output/unclassified.dbn > $output/viroids.dbn
+cat $output/avsunviroidae.rc.dbn $output/pospiviroidae.rc.dbn  $output/unclassified.rc.dbn > $output/viroids.rc.dbn
+cat $output/avsunviroidae.cmscan $output/pospiviroidae.cmscan  $output/unclassified.cmscan > $output/viroids.cmscan
 
 cd $INITIAL_WORKING_DIRECTORY
